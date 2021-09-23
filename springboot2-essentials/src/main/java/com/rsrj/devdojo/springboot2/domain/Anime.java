2f15e9ae1e1004d08a5fbef6271d01420f57786e
rsrj <rocha.railton13@gmail.com>
2021-09-23 18:36:00 -0300

    Annotated class to be identified w/ DB structure

--- springboot2-essentials/src/main/java/com/rsrj/devdojo/springboot2/domain/Anime.java
+++ springboot2-essentials/src/main/java/com/rsrj/devdojo/springboot2/domain/Anime.java
@@ -1,11 +1,22 @@
 package com.rsrj.devdojo.springboot2.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+/*Necessario um construtor sem argumentos para o hibernate*/
+@NoArgsConstructor
+@Entity
 public class Anime {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	/*Por default o Jackson vai mapear pelo nome do atributo
 	 * caso o nome do atributo esteja diferente eh preciso destacar
