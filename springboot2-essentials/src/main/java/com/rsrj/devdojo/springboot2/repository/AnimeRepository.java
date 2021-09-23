2376ab93e9910dec6e13e11685a846abb5878ece
rsrj <rocha.railton13@gmail.com>
2021-09-23 18:45:36 -0300

    Class extended from JpaRepository

--- springboot2-essentials/src/main/java/com/rsrj/devdojo/springboot2/repository/AnimeRepository.java
+++ springboot2-essentials/src/main/java/com/rsrj/devdojo/springboot2/repository/AnimeRepository.java
@@ -1,14 +1,18 @@
 package com.rsrj.devdojo.springboot2.repository;
 
-import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.rsrj.devdojo.springboot2.domain.Anime;
 
 /* Aqui sera representado o banco de dados
  * Todas as querys que serao responsaveis pelo relacionamento
  * com o banco de dados*/
-public interface AnimeRepository {
-	
-	List <Anime> listAll();
+public interface AnimeRepository extends JpaRepository<Anime, Long>{
+	/* No argumento do Jpa se coloca Anime como classe a ser armazenada 
+	 * o atributo que representa o IDe*/	
+	//Jah tem metodos disponiveis no JPA repositorio para listagem
+	//dentre outras funcoes
+	//List <Anime> findAll();
 
+	
 }
