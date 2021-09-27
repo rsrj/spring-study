4cf39eca2bc8646a504668cd7736ce7aba3feb4e
rsrj <rocha.railton13@gmail.com>
2021-09-27 16:15:34 -0300

    Overwriting WebMvcConfigurer to configure pagination default

---
+++ springboot2-essentials/src/main/java/com/rsrj/devdojo/springboot2/configurer/DevDojoWebMvcConfigurer.java
@@ -0,0 +1,22 @@
+package com.rsrj.devdojo.springboot2.configurer;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class DevDojoWebMvcConfigurer implements WebMvcConfigurer {
+
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+		PageableHandlerMethodArgumentResolver pageHandler = new PageableHandlerMethodArgumentResolver();
+		pageHandler.setFallbackPageable(PageRequest.of(1, 5));
+		resolvers.add(pageHandler);
+	}
+
+	
+}
