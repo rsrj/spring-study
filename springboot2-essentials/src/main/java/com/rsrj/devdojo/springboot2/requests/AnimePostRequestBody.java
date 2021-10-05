a7aec628dc4f05902baca6781627a613ab2367fb
rsrj <rocha.railton13@gmail.com>
2021-10-05 17:31:00 -0300

    Added lombok constructors with NoArgs and AllArgs

--- springboot2-essentials/src/main/java/com/rsrj/devdojo/springboot2/requests/AnimePostRequestBody.java
+++ springboot2-essentials/src/main/java/com/rsrj/devdojo/springboot2/requests/AnimePostRequestBody.java
@@ -2,13 +2,17 @@ package com.rsrj.devdojo.springboot2.requests;
 
 import javax.validation.constraints.NotEmpty;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /*Cria uma classe para fazer a resposta e nao usar a classe Anime
  * diretamente no controller*/
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class AnimePostRequestBody {
 	@NotEmpty(message = "The anime name cannot be empty")
 	/*NotEmpty tambem verifica se eh nulo*/
--- springboot2-essentials/src/main/java/com/rsrj/devdojo/springboot2/requests/AnimePutRequestBody.java
+++ springboot2-essentials/src/main/java/com/rsrj/devdojo/springboot2/requests/AnimePutRequestBody.java
@@ -1,11 +1,15 @@
 package com.rsrj.devdojo.springboot2.requests;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class AnimePutRequestBody {
 	private Long id;
 	private String name;
